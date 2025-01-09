69e0580e64dad63c3cab69deac3e9b0f495f8d44
NANOOS105 <heesu105@naver.com>
2025-01-09 12:58:11 +0900

    Initial commit

---
+++ src/test/java/project/plant_report/PlantReportApplicationTests.java
@@ -0,0 +1,13 @@
+package project.plant_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

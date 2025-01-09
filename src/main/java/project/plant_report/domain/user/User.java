e65675a15bc377f4f3bea04598a2d1542d586b66
NANOOS105 <heesu105@naver.com>
2025-01-09 17:28:31 +0900

    user domain 생성

---
+++ src/main/java/project/plant_report/domain/user/User.java
@@ -0,0 +1,32 @@
+package project.plant_report.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String password;
+
+    @Column(nullable = false)
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+}

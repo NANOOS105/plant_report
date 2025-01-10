1147148c3861f93ab50875ed2e2553ec4d4175d8
NANOOS105 <heesu105@naver.com>
2025-01-10 11:19:38 +0900

    plant domain 추가

---
+++ src/main/java/project/plant_report/domain/plant/Plant.java
@@ -0,0 +1,28 @@
+package project.plant_report.domain.plant;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+public class Plant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "plant_id")
+    private int id;
+
+    @Column(name = "plant_name", nullable = false)
+    private String nName;
+
+    @Enumerated(EnumType.STRING) // ENUM 매핑 방식: String 사용
+    @Column(name = "plant_type", nullable = false)
+    private PlantType type;
+
+    @Column(nullable = false)
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+
+}

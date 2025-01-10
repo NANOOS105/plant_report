c93d52694e378dc7dd4aeb35e1e7bc215370a3ab
NANOOS105 <heesu105@naver.com>
2025-01-10 15:30:04 +0900

    SeasonalWateringInterval.java 생성

---
+++ src/main/java/project/plant_report/domain/plant/SeasonalWateringInterval.java
@@ -0,0 +1,25 @@
+package project.plant_report.domain.plant;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class SeasonalWateringInterval {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "plant_id", nullable = false)
+    private Plant plant; // 특정 식물과 연결
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Season season; // 여름, 겨울 (ENUM)
+
+    @Column(nullable = false)
+    private int interval; // 계절별 물주기 간격 (일 단위)
+}
+

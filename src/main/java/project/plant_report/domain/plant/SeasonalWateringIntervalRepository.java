5735e035e568dd66615a326a3dd52d3cd4f7c190
NANOOS105 <heesu105@naver.com>
2025-01-10 15:29:27 +0900

    repository 생성

---
+++ src/main/java/project/plant_report/domain/plant/SeasonalWateringIntervalRepository.java
@@ -0,0 +1,6 @@
+package project.plant_report.domain.plant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeasonalWateringIntervalRepository extends JpaRepository<SeasonalWateringInterval, Long> {
+}

95fd7ff09317778917a83023b6855e613c4ff526
NANOOS105 <heesu105@naver.com>
2025-01-12 22:35:56 +0900

    생성자, season 필드 추가

--- src/main/java/project/plant_report/dto/plant/response/PlantResponseDto.java
+++ src/main/java/project/plant_report/dto/plant/response/PlantResponseDto.java
@@ -1,11 +1,22 @@
 package project.plant_report.dto.plant.response;
 
 import lombok.Getter;
+import project.plant_report.domain.plant.Plant;
+import project.plant_report.domain.plant.Season;
 
 @Getter
 public class PlantResponseDto {
     private Long id;
     private String name;
-    private int wateringInterval;
-    private String nextWateringDate;
+    private int wateringInterval; // 현재 계절에 맞는 물주기 간격
+    private String nextWateringDate; // 다음 물주기 날짜
+    private Season season;
+
+    public PlantResponseDto(Plant plant, int wateringInterval, String nextWateringDate, Season season) {
+        this.id = plant.getId();
+        this.name = plant.getName();
+        this.wateringInterval = wateringInterval;
+        this.nextWateringDate = nextWateringDate;
+        this.season = season;
+    }
 }

f2c16d4b514ef8c148c17b4e407b6f1c6cef2f27
NANOOS105 <heesu105@naver.com>
2025-01-12 22:52:44 +0900

    식물 리스트 조회 추가

--- src/main/java/project/plant_report/service/plant/PlantService.java
+++ src/main/java/project/plant_report/service/plant/PlantService.java
@@ -4,7 +4,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.plant_report.domain.plant.Plant;
 import project.plant_report.domain.plant.PlantRepository;
+import project.plant_report.domain.plant.Season;
 import project.plant_report.dto.plant.request.PlantRequestDto;
+import project.plant_report.dto.plant.response.PlantResponseDto;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PlantService {
@@ -26,4 +32,22 @@ public class PlantService {
         );
 
         plantRepository.save(plant);    }
+
+    @Transactional(readOnly = true)
+    public List<PlantResponseDto> getPlants(Season currentSeason) {
+        return plantRepository.findAll().stream()
+                .map(plant -> {
+                    int wateringInternal = plant.getIntervalForSeason(currentSeason);
+                    String nextWateringDate = calNextWateringDate(wateringInternal);
+                    return new PlantResponseDto(plant, wateringInternal, nextWateringDate, currentSeason);
+                })
+                .collect(Collectors.toList());
+    }
+
+    //다음 물주기 날짜 계산
+    private String calNextWateringDate(int wateringInterval) {
+        LocalDate today = LocalDate.now(); // 오늘 날짜
+        LocalDate nextWateringDate = today.plusDays(wateringInterval); // 물주기 간격만큼 더함
+        return nextWateringDate.toString(); // 날짜를 문자열로 반환
+    }
 }

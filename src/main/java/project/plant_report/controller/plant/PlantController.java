5d42a73dd189ed9cd5dc17627bf9872612ba8112
NANOOS105 <heesu105@naver.com>
2025-01-12 22:32:12 +0900

    조회 컨트롤러 생성

--- src/main/java/project/plant_report/controller/plant/PlantController.java
+++ src/main/java/project/plant_report/controller/plant/PlantController.java
@@ -2,7 +2,10 @@ package project.plant_report.controller.plant;
 
 import org.springframework.web.bind.annotation.*;
 import project.plant_report.dto.plant.request.PlantRequestDto;
+import project.plant_report.dto.plant.response.PlantResponseDto;
 import project.plant_report.service.plant.PlantService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/plant")
@@ -17,5 +20,10 @@ public class PlantController {
     @PostMapping
     public void savePlant(@RequestBody PlantRequestDto request){
         plantService.savePlant(request);
+    }
+
+    @GetMapping
+    public List<PlantResponseDto> getPlants(){
+        return plantService.getPlant();
     }
 }

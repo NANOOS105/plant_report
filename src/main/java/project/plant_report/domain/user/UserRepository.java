d2c89c7d940cbf5837dd83f7e784831321cf2916
NANOOS105 <heesu105@naver.com>
2025-01-13 15:33:18 +0900

    이메일, 닉네임 중복 확인 코드

--- src/main/java/project/plant_report/domain/user/UserRepository.java
+++ src/main/java/project/plant_report/domain/user/UserRepository.java
@@ -3,4 +3,6 @@ package project.plant_report.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+    boolean existsByEmail(String email);
+    boolean existsByName(String name);
 }

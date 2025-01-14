1ebd33d62bacb58a6fa409707d296cea94313159
NANOOS105 <heesu105@naver.com>
2025-01-14 16:59:46 +0900

    lastWateringDate 필드 추가

--- src/main/java/project/plant_report/dto/plant/request/PlantSaveRequestDto.java
+++ src/main/java/project/plant_report/dto/plant/request/PlantSaveRequestDto.java
@@ -2,10 +2,13 @@ package project.plant_report.dto.plant.request;
 
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @Getter
 public class PlantSaveRequestDto {
     private String name; // 식물 이름
     private int commonInterval; // 공통 물주기
     private int summerInterval; // 여름 물주기 (선택적)
     private int winterInterval; // 겨울 물주기 (선택적)
+    private LocalDate lastWateringDate; //마지막으로 물 준 날짜 (선택적)
 }

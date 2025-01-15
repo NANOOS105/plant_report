8e7a1351353cfadc0db7f2cbf1c6403470640e9f
NANOOS105 <heesu105@naver.com>
2025-01-15 14:20:42 +0900

    user 필드 추가

--- src/main/java/project/plant_report/dto/plant/request/PlantSaveRequestDto.java
+++ src/main/java/project/plant_report/dto/plant/request/PlantSaveRequestDto.java
@@ -1,11 +1,14 @@
 package project.plant_report.dto.plant.request;
 
 import lombok.Getter;
+import project.plant_report.domain.user.User;
 
 import java.time.LocalDate;
 
 @Getter
 public class PlantSaveRequestDto {
+
+    private User user;
     private String name; // 식물 이름
     private int commonInterval; // 공통 물주기
     private int summerInterval; // 여름 물주기 (선택적)
